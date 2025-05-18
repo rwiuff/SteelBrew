ee61cbf458cefde0a24380e50dd05f6e968234a2
Rasmus Wiuff <rwiuff@gmail.com>
2025-05-18 23:08:26 +0200

    Made a cool test

--- app/src/main/java/org/rwiuff/steelbrew/Driver.java
+++ app/src/main/java/org/rwiuff/steelbrew/Driver.java
@@ -12,16 +12,50 @@ public class Driver {
     public static void main(String[] args) {
         SteelBrew steelBrew = new SteelBrew();
         steelBrew.clean();
-        runDUT();
-        steelBrew.burn();
-        peekPokeStep();
-        steelBrew.burn();
-        expect();
-        steelBrew.burn();
-        Assert();
-        steelBrew.burn();
-        concurrency();
-        steelBrew.clean();
+        // runDUT();
+        // steelBrew.burn();
+        // peekPokeStep();
+        // steelBrew.burn();
+        // expect();
+        // steelBrew.burn();
+        // Assert();
+        // steelBrew.burn();
+        // concurrency();
+        // steelBrew.burn();
+        coolDemo();
+        // steelBrew.clean();
+    }
+
+    private static void coolDemo() {
+        Forge.enableWSL(true);
+        Brewer alu = new Brewer("alu");
+        alu.clocks(20);
+        Signal a_in = new Signal("a_in", 1);
+        Signal b_in = new Signal("b_in", 1);
+        Batch batch1 = new Batch("FirstTest");
+        batch1.addSignal(a_in);
+        batch1.addSignal(b_in);
+        batch1.Assert(a_in, b_in, 1, Operator.neq, alu);
+        batch1.peek(a_in);
+        batch1.peek(b_in);
+        batch1.poke(a_in, BigInteger.TWO);
+        batch1.step();
+        batch1.peek(a_in);
+
+        Batch batch2 = new Batch("SecondTest");
+        batch2.addSignal(a_in);
+        batch2.addSignal(b_in);
+        batch2.Assert(a_in, b_in, 1, Operator.neq, alu);
+        batch2.peek(a_in);
+        batch2.peek(b_in);
+        batch2.poke(b_in, BigInteger.TWO);
+        batch2.step();
+        batch2.poke(a_in, BigInteger.TWO);
+        batch2.peek(a_in);
+
+        alu.brew(batch1);
+        alu.brew(batch2);
+        Forge.simulate();
     }
 
     public static void runDUT() {
